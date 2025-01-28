8a36757e646b87f397065652811e99f993f0a2b9
Pietro <pietro.gerletti@pm.me>
2025-01-28 12:05:06 +0100

    version 0.2.3

---
+++ tests/test_HPC_runme.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan 25 11:07:15 2025
+
+@author: pietro
+"""
+from simplicity.runme import run_experiment
+from tests.local_runme import test_experiment_output
+import simplicity.runners.slurm
+
+## fixture  experiment settings (sm.write_settings arguments)
+def fixture_experiment_settings():
+    parameters      = {}
+    n_seeds         = 10
+    return (parameters, n_seeds)
+
+##### <actual test>
+def test_run_experiment_HPC(test_number:int):
+
+    print('')
+    print('##########################################')
+    print('testing HPC runner')
+    print('##########################################')
+    experiment_name = f'test_HPC_experiment_#{test_number}'
+    try:
+        run_experiment(experiment_name, 
+                       fixture_experiment_settings,             
+                       simplicity_runner  = simplicity.runners.slurm,
+                       plot_trajectory = True,
+                       archive_experiment = False)
+    except:
+        raise RuntimeError('The code did not pass the running test')
+    print('')
+    print(f'TEST HPC RUNNER #{test_number} -- SUCCESS.')
+    print('##########################################')
+    return experiment_name
+    
+def test_experiment_HPC(test_number:int):
+    test_experiment_output(test_run_experiment_HPC(test_number))
+    
+##### </actual test>
+
+if __name__ == "__main__":
+    # test_experiment_local('serial',1)
+    test_experiment_HPC(1)
