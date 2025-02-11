532755833ddfd3044227d70dff4cfabc3c420c60
Pietro <pietro.gerletti@pm.me>
2025-02-11 11:52:56 +0100

    version 0.3.3

--- tests/test_u_fitting.py
+++ tests/test_u_fitting.py
@@ -10,40 +10,53 @@ import numpy as np
 import matplotlib.pyplot as plt
 from lmfit import Model
 import simplicity.output_manager as om
+import argparse 
 
-# Read data from CSV
-experiment_name = 'test_param_space_3'
-data = om.read_u_e_values(experiment_name)
-x_data = data['evolutionary_rate'] 
-y_data = data['u']  
-
-# Define the logarithmic model
+# Define the model
 def log_model(x, A, B, C, D):
     return A * np.log(B * x + C) + D
 
-# Create the Model
-model = Model(log_model)
+def fit_log_u(experiment_name):
+    # Read data from CSV
+    data = om.read_u_e_values(experiment_name)
+    x_data = data['evolutionary_rate'] 
+    y_data = data['u']  
 
-# Set initial parameter guesses (A, B, C, D)
-params = model.make_params(A=3, B=2, C=1, D=2)
+    # Create the Model
+    model = Model(log_model)
+    
+    # Set initial parameter guesses 
+    params = model.make_params(A=3, B=2, C=1, D=2)
+    
+    # Set boundaries for parameters
+    params['A'].set(min=0)  
+    params['B'].set(min=0.000001)  
+    params['C'].set(min=0.000001)
+    
+    # Fit the model to the data
+    result = model.fit(y_data, params, x=x_data)
+    
+    # Print the fit results
+    print(result.fit_report())
+    
+    # Plot the original data and the fitted curve
+    plt.scatter(x_data, y_data, label='Data', color='blue', alpha=0.5)
+    plt.plot(x_data, result.best_fit, label='Fitted curve', color='red', linewidth=2)
+    plt.xlabel('Evolutionary Rate')
+    plt.xlim(0)
+    plt.ylim(0)
+    plt.ylabel('u')
+    plt.legend()
+    plt.title('Logarithmic Fit to Data')
+    plt.show()
 
-# Set boundaries for parameters
-params['A'].set(min=0)  
-params['B'].set(min=0.000001)  
-params['C'].set(min=0.000001)
-
-
-# Fit the model to the data
-result = model.fit(y_data, params, x=x_data)
-
-# Print the fit results
-print(result.fit_report())
-
-# Plot the original data and the fitted curve
-plt.scatter(x_data, y_data, label='Data', color='blue', alpha=0.5)
-plt.plot(x_data, result.best_fit, label='Fitted curve', color='red', linewidth=2)
-plt.xlabel('Evolutionary Rate')
-plt.ylabel('u')
-plt.legend()
-plt.title('Logarithmic Fit to Data')
-plt.show()
+def main():
+    # Set up the argument parser
+    parser = argparse.ArgumentParser(description="Run script to fit curve to u data")
+    parser.add_argument('experiment_name', type=int, help="experiment name")
+    args = parser.parse_args()
+    # Run the script with the provided parameter
+    fit_log_u(args)
+    
+if __name__ == "__main__":
+    main()
