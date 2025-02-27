cccdcfe59960229771566aef978b130330f435e7
Pietro <pietro.gerletti@pm.me>
2025-02-27 19:25:02 +0100

    version 0.7.2

---
+++ scripts/plots/plot_fit_observed_evolutionary_rate_u.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 10 13:20:25 2025
+
+@author: pietro
+"""
+import simplicity.plots_manager as pm 
+import simplicity.tuning.evolutionary_rate as er
+import argparse 
+
+# def u_estimator(fit_result,e):
+#     A = fit_result.params['A'].value
+#     B = fit_result.params['B'].value
+#     C = fit_result.params['C'].value
+    
+#     return A * np.log(B * e + C)
+
+def main():
+    # Set up the argument parser
+    parser = argparse.ArgumentParser(description="Run script to fit different curves to u data")
+    parser.add_argument('experiment_name', type=str, help="experiment name")
+    args = parser.parse_args()
+    
+    model_types = ['linear',
+                   'log',
+                   'exp',
+                   'double_log',
+                   'tan']
+    
+    for model_type in model_types:
+        print('')
+        print('###############################################################')
+        print('')
+        print(f'Fitting {model_type} to evolutionary rate data')
+        print('')
+        print('###############################################################')
+        print('')
+        fit_result = er.fit_observed_evolutionary_rate(args.experiment_name, model_type)
+        print(f'saving plot in {args.experiment_name}/04_Output/')
+        pm.plot_observed_evolutionary_rate_fit(args.experiment_name, 
+                                               fit_result, 
+                                               model_type)
+        print('')
+        print('###############################################################')
+        print('')
+    
+if __name__ == "__main__":
+    main()
