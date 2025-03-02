c66a0915a1cf5d16e814632628387632ed6b130d
Pietro <pietro.gerletti@pm.me>
2025-03-02 13:21:54 +0100

    version 0.8.1

--- scripts/fit_observed_evolutionary_rate_regressor.py
+++ scripts/fit_observed_evolutionary_rate_regressor.py
@@ -18,7 +18,7 @@ def fit_models(experiment_name, model_types, data_type):
         # build the dataframe needed for the fit
         om.build_combined_observed_evolutionary_rate_vs_parameter_df(experiment_name, 
                                                                     parameter, 
-                                                                    min_sim_lenght=0)
+                                                                    min_sim_lenght=120)
          # import the df needed for the fit
         df = om.read_combined_observed_evolutionary_rate_csv(experiment_name, parameter)
         # select plot function
@@ -28,7 +28,7 @@ def fit_models(experiment_name, model_types, data_type):
         # build the dataframe needed for the fit
         om.build_combined_observed_evolutionary_rate_vs_parameter_df(experiment_name, 
                                                                     parameter, 
-                                                                    min_sim_lenght=0)
+                                                                    min_sim_lenght=120)
         om.build_observed_evolutionary_rates_vs_parameter_df(experiment_name, 
                                                                     parameter, 
                                                                     min_sim_lenght=120)
