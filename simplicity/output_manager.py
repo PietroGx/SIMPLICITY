a21b1f7519e95576bbe58765726395bab3514f08
Pietro <pietro.gerletti@pm.me>
2025-03-05 14:49:15 +0100

    version 0.9.0

--- simplicity/output_manager.py
+++ simplicity/output_manager.py
@@ -496,6 +496,8 @@ def write_fit_results_csv(experiment_name, model_type, fit_result):
 def read_fit_results_csv(experiment_name, model_type):
     fit_results = get_fit_results_filepath(experiment_name, model_type)
     df = pd.read_csv(fit_results,index_col=0,dtype={"Best Fit": float})
+    print("#########################################################")
+    print(df.dtypes)
     return df
     
 
