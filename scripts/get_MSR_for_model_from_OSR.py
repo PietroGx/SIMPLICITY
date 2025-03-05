960c281d45e9ed03f80bf37facfb1cd18c45ebfe
Pietro <pietro.gerletti@pm.me>
2025-03-05 14:58:07 +0100

    version 0.9.0

--- scripts/get_MSR_for_model_from_OSR.py
+++ scripts/get_MSR_for_model_from_OSR.py
@@ -13,6 +13,8 @@ def get_MSR_for_model_from_OSR(experiment_name, OSR):
     model_type = 'log'
     fit_results_params_df = om.read_fit_results_csv(experiment_name, model_type)
     params = fit_results_params_df.to_dict()
+    params = {k: float(v) for k, v in params.items()}
+
     print(params)
     for i in params:
         print(type(i))
