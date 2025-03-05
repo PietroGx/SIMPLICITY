fb1fe772d0077c21903df51ef9ba4f069cf8ce26
Pietro <pietro.gerletti@pm.me>
2025-03-05 16:15:49 +0100

    version 0.9.0

---
+++ scripts/generate_data_OSR_fit.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+
+@author: pietro
+
+STANDARD_VALUES for SIMPLICITY simulation: 
+    
+    STANDARD_VALUES = {
+        "population_size": 1000,
+        "tau_3": 7.5,
+        "infected_individuals_at_start": 10,
+        "R": 1.3,
+        "diagnosis_rate": 0.1,             # in percentage, will be converted to kd in model 
+        "IH_virus_emergence_rate": 0,      # k_v in theoretical model equations
+        "molecular_substitution_rate": 0.0017,       # e in theoretical model equations
+        "final_time": 365,
+        "max_runtime": 259200, 
+        "phenotype_model": 'immune waning',  # or 'distance from wt'
+        "sequencing_rate": 0.05,
+        "seed": None,
+        "F": 1.25
+    }
+    
+y, you can specify them in the parameters dictionary below. 
+For each parameter, specify a list of values that you would like to use for the 
+simulation. If you want to change more than one parameter at the time, consider 
+that you need to enter the same number of values for each parameter, e.g. :
+    par 1 = [value1, value2]
+    par 2 = [value3, value4]
+This will run a simulation with par 1 = value1 and par 2 = value 3, and a simulation
+with par 1 = value2 and par 2 = value4. 
+
+Each simulation will be repeated n_seeds time with a different random seed.
+
+The set of all simulations is what we call an experiment.
+"""
+from experiment_script_runner import run_experiment_script
+import argparse
+import numpy as np
+experiment_name =  'generate_data_OSR_fit'
+
+def fixture_experiment_settings():
+
+    # number of values for MSR
+    molecular_substitution_rate_num_values = 15
+    
+    # Generate values spaced logarithmically between 10^-5 and 10^-2
+    values = np.logspace(np.log10(1e-5), np.log10(0.01), 
+                         num=molecular_substitution_rate_num_values)
+    molecular_substitution_rate_values = values.tolist()
+    # mapped_sim_lenght = np.logspace(np.log10(3650), np.log10(365), num=molecular_substitution_rate_num_values).tolist()
+
+    
+    parameters      = {'molecular_substitution_rate': molecular_substitution_rate_values,
+                       'infected_individuals_at_start': [100]*molecular_substitution_rate_num_values,
+                       'final_time': [365*3]*molecular_substitution_rate_num_values
+                       }
+    n_seeds = 100
+
+    return (parameters, n_seeds)
+
+def main():
+    # Set up the argument parser
+    parser = argparse.ArgumentParser(description="Run script to generate IH lineages data")
+    parser.add_argument('runner', type=str, help="runner")
+    parser.add_argument('experiment_number', type=int, help="experiment number")
+    args = parser.parse_args()
+    # Run the script 
+    run_experiment_script(args.runner, 
+                          args.experiment_number, 
+                          fixture_experiment_settings,
+                          experiment_name)
+
+if __name__ == "__main__":
+    main()
+    
+    
---
+++ scripts/generate_user_set_parameters_file.py
@@ -0,0 +1,149 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar  5 12:51:52 2025
+
+@author: pietro
+"""
+
+import sys
+import simplicity.tuning.evolutionary_rate as er
+import simplicity.settings_manager as sm
+import simplicity.output_manager as om
+
+def get_MSR_for_model_from_OSR(experiment_name, OSR):
+    model_type = 'log'
+    fit_results_params_df = om.read_fit_results_csv(experiment_name, model_type)
+    params = fit_results_params_df.to_dict()
+
+    MSR = er.inverse_log_regressor(OSR, params)
+   
+    return MSR
+
+def get_MSR_for_model_from_OSR_using_reference(OSR):
+    pass
+
+def get_MSR():
+    ''' Prompts user to give observed evolutionary rate for the simulation and returns the correct MSR parameter
+    '''
+    while True:
+        print('')
+        print('-----')
+        print('Entering setup of substitution rate for SIMPLICITY')
+        print('-----')
+        print('')
+        check_experiment_run = input("Did you run generate_data_OSR_fit.py? (y/n/q): ").strip().lower()
+        check_fit_run = input("Did you run plot_OSR_fit.py? (y/n/q): ").strip().lower()
+        
+        if check_experiment_run == "y" and check_fit_run == "y":
+            experiment_name = input("Enter the name of the experiment used for the observed substitution rate (OSR) fit: ")
+            print('')
+            print('The observed substitution rate (OSR) value should be within the range obtained from the fitting.')
+            print(f'If you are unsure, check the fit plot (Data/{experiment_name}/figure3A_OSR_log_fit.png)')
+            
+            while True:
+                OSR = input("Enter the desired observed substitution rate (OSR) value: ")
+                try:
+                    OSR_value = float(OSR)
+                    return get_MSR_for_model_from_OSR(experiment_name, OSR_value)
+                except Exception as e:
+                    print(e)
+                    sys.exit() 
+                    
+        elif check_experiment_run == "n" or check_fit_run == "n":
+            print('')
+            print('If you did not run generate_data_OSR_fit.py or fitted the curve, we will use the repo provided values.')
+            print('----------------------')
+            
+            while True:
+                OSR = input("Enter the desired observed substitution rate (OSR) value (OSR must be between 0.0001 and 0.001): ")
+                try:
+                    OSR_value = float(OSR)
+                    if 0.0001 <= OSR_value <= 0.001:
+                        return get_MSR_for_model_from_OSR_using_reference(OSR_value)  
+                    else:
+                        print("Invalid OSR value. It must be between 0.0001 and 0.001. Please reenter value.")
+                except Exception as e:
+                    print(e)
+                    sys.exit() 
+
+        elif check_experiment_run == "q":
+            print("Exiting...")
+            sys.exit()
+
+        else:
+            print("Invalid input, please select 'y', 'n', or 'q' to quit.")
+
+
+def get_valid_input(prompt, expected_type, standard_value, min_value=None, max_value=None):
+    while True:
+        user_input = input(f"{prompt} (Press Enter to keep standard value: {standard_value}) or type 'q' to quit: ").strip()
+        if user_input.lower() == 'q':
+            print("Process exited.")
+            sys.exit()
+        if user_input == "":
+            print("Keeping standard value.")
+            return standard_value
+        try:
+            if expected_type == int:
+                value = int(user_input)
+                if min_value is not None and value < min_value:
+                    print(f"Value must be at least {min_value}.")
+                    continue
+                if max_value is not None and value > max_value:
+                    print(f"Value must be at most {max_value}.")
+                    continue
+            elif expected_type == float:
+                value = float(user_input)
+                if min_value is not None and value < min_value:
+                    print(f"Value must be at least {min_value}.")
+                    continue
+                if max_value is not None and value > max_value:
+                    print(f"Value must be at most {max_value}.")
+                    continue
+            else:
+                value = str(user_input)
+            return value
+        except ValueError:
+            print(f"Please enter a valid {expected_type.__name__} value.")
+
+
+def get_user_input():
+    params = {}
+    standard_values = sm.read_standard_parameters_values()
+    parameter_specs = sm.read_parameter_specs()
+    
+    no_prompt_params = {"seed","F"}  # Parameters not prompted to the user
+    
+    print("""
+    Welcome! This program will guide you through creating a settings file for running your SIMPLICITY simulations.
+    Input the prompted parameters and we will generate a configuration file for you.
+    """)
+    
+    for key, standard_value in standard_values.items():
+        param_spec = parameter_specs.get(key, {})
+        expected_type = eval(param_spec.get("type", "str"))  # Convert type from string
+        min_value = param_spec.get("min")
+        max_value = param_spec.get("max")
+        
+        if key in no_prompt_params:
+            params[key] = standard_value  # Automatically set from standard values
+        elif key == 'molecular_substitution_rate':
+            params[key] = get_MSR()
+        else:
+            print('')
+            prompt = f"Please enter {key} value"
+            params[key] = get_valid_input(prompt, expected_type, standard_value, min_value, max_value)
+    
+    return params
+
+def main():
+    user_set_parameters = get_user_input()
+    filename = input("Enter the filename to save the settings (default: user_parameters.json): ").strip()
+    filename = filename + '.json'
+    if not filename:
+        filename = "user_parameters.json"
+    sm.write_user_set_parameters_file(user_set_parameters, filename)
+    
+if __name__ == "__main__":
+    main()
