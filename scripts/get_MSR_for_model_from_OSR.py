9fdf2bb15df420e54010ea2b0dbcab55de9e0c8a
Pietro <pietro.gerletti@pm.me>
2025-03-05 14:42:46 +0100

    version 0.9.0

--- scripts/get_MSR_for_model_from_OSR.py
+++ scripts/get_MSR_for_model_from_OSR.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/uparser.add_argument('experiment_name', type=str, help="experiment name")sr/bin/env python3
 # -*- coding: utf-8 -*-
 """
 Created on Tue Mar  4 15:34:12 2025
@@ -21,9 +21,10 @@ def main():
     # Set up the argument parser
     parser = argparse.ArgumentParser(description="Plot")
     parser.add_argument('experiment_name', type=str, help="experiment name")
+    parser.add_argument('OSR', type=str, help="desired observed substitution rate for simulation")
     args = parser.parse_args()
     
-    get_MSR_for_model_from_OSR(args.experiment_name)
+    get_MSR_for_model_from_OSR(args.experiment_name, args.OSR)
     
     
 if __name__ == "__main__":
