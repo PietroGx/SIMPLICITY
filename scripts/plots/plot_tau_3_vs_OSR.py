e20705ea0dfa3469ac456f455ec7d511f57efcce
Pietro <pietro.gerletti@pm.me>
2025-03-07 13:21:54 +0100

    version 0.10.0

--- scripts/plots/plot_tau_3_vs_OSR.py
+++ scripts/plots/plot_tau_3_vs_OSR.py
@@ -13,10 +13,25 @@ def main():
     parser = argparse.ArgumentParser(description="Plot")
     parser.add_argument('experiment_name', type=str, help="experiment name")
     args = parser.parse_args()
+    
+    parameter = 'tau_3'
+    min_seq_number = 0
+    min_sim_lenght = 0
+    
+    # build the dataframe needed for the plot
+    om.write_combined_OSR_vs_parameter_csv(experiment_name, 
+                                            parameter, 
+                                            min_seq_number,
+                                            min_sim_lenght)
+    om.write_OSR_vs_parameter_csv(experiment_name, 
+                                    parameter, 
+                                    min_seq_number,
+                                    min_sim_lenght)
     # Run the script with the provided parameter
-    pm.plot_combined_OSR_vs_parameter(args.experiment_name,'tau_3', 
-                                      min_seq_number=0, 
-                                      min_sim_lenght=0)
+    pm.plot_combined_OSR_vs_parameter(args.experiment_name,
+                                      parameter, 
+                                      min_seq_number, 
+                                      min_sim_lenght)
     
 if __name__ == "__main__":
     main()
