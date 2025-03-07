6d3cd64c54d58cf5dc7ce01deabc0a3d82b0be46
Pietro <pietro.gerletti@pm.me>
2025-03-07 11:54:06 +0100

    version 0.10.0

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('LICENSE') as f:
 
 setup(
     name='simplicity',
-    version='0.9.1',
+    version='0.10.0',
     description='Stochastic sIMulation of SARS-CoV-2 sPread and evoLutIon aCcountIng for within-hosT dYnamics (SIMPLICITY)',
     long_description=readme,
     author='Pietro Gerletti, Jean-Baptiste Escudié',
