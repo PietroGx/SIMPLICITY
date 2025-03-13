0054524cb60439d12f72e970e5d8ec1f09686f6e
Pietro <pietro.gerletti@pm.me>
2025-03-13 02:16:45 +0100

    version 0.11.1

--- scripts/plots/plot_R_eff.py
+++ scripts/plots/plot_R_eff.py
@@ -7,7 +7,7 @@ import simplicity.dir_manager as dm
 import argparse
 import os
 
-def select_and_plot_R_effective(experiment_name, selected_seeds):
+def select_and_plot_R_effective(experiment_name, selected_seeds, lin_threshold):
     SSODs = []
     for simulation_output_dir in dm.get_simulation_output_dirs(experiment_name):
         SSODs += (dm.get_seeded_simulation_output_dirs(simulation_output_dir))
@@ -19,10 +19,13 @@ def select_and_plot_R_effective(experiment_name, selected_seeds):
                     seeded_simulation_output_dir = ssod
                     # set window size and lineage occurence threshold
                     window_size = 14
-                    threshold = 0.1
                     # process R effective data and write csv files needed for plot
-                    om.write_R_effective_dfs_csv(experiment_name, seeded_simulation_output_dir, window_size, threshold)
-                    pm.plot_R_effective(experiment_name, seeded_simulation_output_dir, window_size, threshold)
+                    om.write_R_effective_dfs_csv(experiment_name, 
+                                                 seeded_simulation_output_dir, 
+                                                 window_size, lin_threshold)
+                    pm.plot_R_effective(experiment_name, 
+                                        seeded_simulation_output_dir, 
+                                        window_size, lin_threshold)
 
 def main():
     # Set up the argument parser
@@ -31,7 +34,8 @@ def main():
     parser.add_argument('selected_seeds', nargs='+', type=int, help="List of selected seeds (plot relative simulation)")
     args = parser.parse_args()
     # Run the script 
-    select_and_plot_R_effective(args.experiment_name, args.selected_seeds)
+    lin_threshold = 0
+    select_and_plot_R_effective(args.experiment_name, args.selected_seeds, lin_threshold)
     
 if __name__ == "__main__":
     main()
