910e31df9ade343803cf366654c3d0669156c3d0
Pietro <pietro.gerletti@pm.me>
2025-03-13 02:32:22 +0100

    version 0.11.1

--- scripts/plots/plot_R_eff.py
+++ scripts/plots/plot_R_eff.py
@@ -7,18 +7,17 @@ import simplicity.dir_manager as dm
 import argparse
 import os
 
-def select_and_plot_R_effective(experiment_name, selected_seeds, lin_threshold):
+def select_and_plot_R_effective(experiment_name, window_size, lin_threshold, selected_seeds):
     SSODs = []
+    # get all the seeded simulation output directories
     for simulation_output_dir in dm.get_simulation_output_dirs(experiment_name):
         SSODs += (dm.get_seeded_simulation_output_dirs(simulation_output_dir))
-        
+        # loop over the ssods and only plot the selected seeds folders
         for ssod in SSODs:
             seed = os.path.basename(ssod)
             for selected_seed in selected_seeds:
                 if seed == f"seed_{selected_seed:04d}":
                     seeded_simulation_output_dir = ssod
-                    # set window size and lineage occurence threshold
-                    window_size = 14
                     # process R effective data and write csv files needed for plot
                     om.write_R_effective_dfs_csv(experiment_name, 
                                                  seeded_simulation_output_dir, 
@@ -31,11 +30,16 @@ def main():
     # Set up the argument parser
     parser = argparse.ArgumentParser(description="Plot")
     parser.add_argument('experiment_name', type=str, help="experiment name")
+    parser.add_argument('window_size', type=int, help="time window_size")
+    parser.add_argument('lin_threshold', type=str, help="lineage occurence threshold")
     parser.add_argument('selected_seeds', nargs='+', type=int, help="List of selected seeds (plot relative simulation)")
     args = parser.parse_args()
+    
     # Run the script 
-    lin_threshold = 0
-    select_and_plot_R_effective(args.experiment_name, args.selected_seeds, lin_threshold)
+    select_and_plot_R_effective(args.experiment_name, 
+                                args.window_size,
+                                args.lin_threshold,
+                                args.selected_seeds)
     
 if __name__ == "__main__":
     main()
