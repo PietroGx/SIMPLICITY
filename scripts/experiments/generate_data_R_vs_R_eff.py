8605e8452c7b01a534e3748e3eff0e4f9baa83be
Pietro <pietro.gerletti@pm.me>
2025-04-04 22:30:17 +0200

    Version 0.16.06

--- scripts/experiments/generate_data_R_vs_R_eff.py
+++ scripts/experiments/generate_data_R_vs_R_eff.py
@@ -17,12 +17,13 @@ def fixture_experiment_settings():
     
     varying_params = {
         'R': [0.8,1,2,4,8],
-        'diagnosis_rate': [0],
+        # 'diagnosis_rate': [0],
         'phenotype_model': ['linear', 'immune_waning']
     }
 
     fixed_params = {
         'infected_individuals_at_start': 100,
+        'diagnosis_rate': 0,
         'final_time': 365*3
     }
     
