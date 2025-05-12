af34a503dee9edab0f239e1bdc6789c14cc494c6
Pietro <pietro.gerletti@pm.me>
2025-05-12 15:26:22 +0200

    Version 0.18.00

--- simplicity/plots_manager.py
+++ simplicity/plots_manager.py
@@ -252,7 +252,7 @@ def plot_figure_tempest_regression(experiment_name):
     parameter = 'nucleotide_substitution_rate'
     experiment_plots_dir = dm.get_experiment_plots_dir(experiment_name)
     simulation_output_dirs = dm.get_simulation_output_dirs(experiment_name)
-    simulation_output_dir = simulation_output_dirs[0]
+    simulation_output_dir = simulation_output_dirs[10]
     param = sm.get_parameter_value_from_simulation_output_dir(simulation_output_dir, parameter)
     sequencing_data_df = om.create_combined_sequencing_df(simulation_output_dir, 
                                                           min_seq_number=30,
